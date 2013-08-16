8d7bf6ff366a74f9e5f57ab4f7f560ee259df3ac
Michael Tieso <vskylabv@gmail.com>
2013-08-15 21:17:08 -0400

    Add Username Field to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
+
+  # Validate Uniqueness of Username
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
